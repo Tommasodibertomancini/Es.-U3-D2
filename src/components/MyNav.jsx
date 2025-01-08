9a66adac6037e209dc144c1f4150e177df475a99
Tommasodibertomancini <tommydbm@gmail.com>
2025-01-08 14:45:39 +0100

    footer

---
+++ src/components/MyNav.jsx
@@ -0,0 +1,25 @@
+import { Navbar, Nav, Container } from 'react-bootstrap'
+
+const MyNav = function () {
+  return(
+  <Navbar
+    expand="md"
+    bg="dark"
+    data-bs-theme="dark"
+  >
+    <Container fluid>
+      <Navbar.Brand href="#">EPIBOOKS</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto">
+          <Nav.Link href="#">Home</Nav.Link>
+          <Nav.Link href="#">About</Nav.Link>
+          <Nav.Link href="#">Browse</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  )
+}
+
+export default MyNav
